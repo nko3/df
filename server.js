0ec387114822adbea16ad7e7e9715f3b9e1f52de
Tõnis Tiigi <tonistiigi@gmail.com>
2012-11-10 07:12:30 +0200

    Express base

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000);
+var express = require('express')
 
-console.log('Server running at http://0.0.0.0:8000/');
+var conf = global.conf = require('rc')(require('./package').name, {
+  port: 3300
+})
+
+
+var app = express()
+app.use(express.bodyParser())
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.listen(conf.port)
+console.log('Server started at http://localhost:' + conf.port)
+
+app.use(express.static(__dirname + '/static'))
+
+app.get('/', function(req, res) {
+  res.render('fp')
+})
+
