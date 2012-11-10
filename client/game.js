6b5c95545b5b5370582ae8995d0719fd1fc43815
Tõnis Tiigi <tonistiigi@gmail.com>
2012-11-10 09:27:50 +0200

    Game network methods

--- client/game.js
+++ client/game.js
@@ -5,3 +5,25 @@ function Game() {
   
 }
 inherits(Game, EventEmitter)
+
+Game.prototype.join = function(playerdata, cb) {
+  // playerdata -> {name, avatar}
+  // cb -> f(err, playerId)
+}
+
+Game.prototype.sendSolution = function(solution, cb) {
+  // solution -> {index1: val1, index2: val2}
+  // cb -> f(err)
+}
+
+Game.prototype.setStartTime = function(time, cb) {
+  // time - in seconds
+  // cb -> f(err)
+}
+
+/* //(optional)
+Game.prototype.increaseTimeout = function(timeout, cb) {
+  // timeout - in seconds
+  // cb -> f(err)
+}
+*/
