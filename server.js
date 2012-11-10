58024f7cc821a20b02141f0a7537b987bef0537c
Tõnis Tiigi <tonistiigi@gmail.com>
2012-11-10 15:07:36 +0200

    Rooms persistence

--- server.js
+++ server.js
@@ -27,6 +27,13 @@ app.get('/', function(req, res) {
 })
 
 var rooms = require('./data/rooms')
+var kv = require('kv')('/tmp/kv')
+kv.has('rooms', function(err, stat){
+  if (!err && stat) {
+    kv.get('rooms').pipe(rooms)
+  }
+  rooms.replicateStream().pipe(kv.put('rooms'))
+})
 
 function uniqueId() {
   return Math.floor(Math.random() * 1e8).toString(16)
